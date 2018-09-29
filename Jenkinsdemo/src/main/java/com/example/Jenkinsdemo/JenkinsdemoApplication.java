ad151a60d3a1573232eb2ff5a726ba3eff49f2be
shubham.shirore <shubham.shirore@Dev12.glodom.local>
2018-09-29 11:45:11 +0530

    ty

--- Jenkinsdemo/src/main/java/com/example/Jenkinsdemo/JenkinsdemoApplication.java
+++ Jenkinsdemo/src/main/java/com/example/Jenkinsdemo/JenkinsdemoApplication.java
@@ -2,11 +2,28 @@ package com.example.Jenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jmx.export.MBeanExporter;
 
 @SpringBootApplication
-public class JenkinsdemoApplication {
+public class JenkinsdemoApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsdemoApplication.class, args);
 	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JenkinsdemoApplication.class);
+    }
+
+  @Bean
+	 public MBeanExporter exporter()
+	 {
+	     final MBeanExporter exporter = new MBeanExporter();
+	     exporter.setAutodetect(true);
+	     exporter.setExcludedBeans("dataSource");
+	     return exporter;
+	 }
 }
